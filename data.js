eb328b689f731b95b2bd3c0a24ecf7ed674bd35c
yashmonk123 <110120859+yashmonk123@users.noreply.github.com>
2023-03-02 23:03:32 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'Bullock Cart',
+      slug: '',
+      category: 'karnatak toys',
+      image: '/images/p1.png', // 679px × 829px
+      price: 10,
+      countInStock: 10,
+      brand: 'handcrafted',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Chariot',
+      slug: '',
+      category: 'odisha toys',
+      image: '/images/p2.png',
+      price: 20,
+      countInStock: 20,
+      brand: 'handcrafted toy',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      name: 'veena',
+      slug: '',
+      category: 'music',
+      image: '/images/p6.png',
+      price: 25,
+      countInStock: 15,
+      brand: 'handcrafted toys',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+    {
+      name: 'Dancing doll',
+      slug: '',
+      category: 'karnatak toys',
+      image: '/images/p4.png',
+      price: 15,
+      countInStock: 5,
+      brand: 'handcrafted toys',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+  ],
+};
+export default data;
